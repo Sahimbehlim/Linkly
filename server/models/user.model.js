769b6ba7281db4c4f7d0e5e392c1bb9ec942ccda
Behlim Shaeem <behlimsahim@gmail.com>
2025-05-22 08:52:13 +0530

    add user model with necessary methods

---
+++ server/models/user.model.js
@@ -0,0 +1,52 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, "Fullname is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+// Hash the password
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  }
+});
+
+// Compare hashed password and user's given password
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// Generate accessToken
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.TOKEN_EXPIRY }
+  );
+};
+
+export const User = mongoose.models.User || mongoose.model("User", userSchema);
